cad01c05c71547354a1ab539d8adec41c3da33d4
ZENOS <rajdeeppal6057@gmail.com>
2022-11-12 18:28:27 +0530

    rajdeep.c

---
+++ rajdeep.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+
+{
+    int a,b,c;
+    printf("enter the valu= ");
+    scanf("%d %d",&a,&b);
+    printf("the sum is %d + %d = %d",a,b,c=a+b);
+
+
+
+    return 0;
+}
